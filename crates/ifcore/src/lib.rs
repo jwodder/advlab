1458db6747e5d3cd3353d94aaebd722f861465d3
John T. Wodder II <git@varonathe.org>
2025-05-30 14:36:58 -0400

    Core code

--- crates/ifcore/src/lib.rs
+++ crates/ifcore/src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::io::{self, BufRead};
+
+pub trait GameBuilder: Sized {
+    type Engine: GameEngine;
+
+    fn start(self) -> Output<Self::Engine>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait GameEngine: Sized {
+    fn handle_input(self, input: &str) -> Output<Self>;
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Output<G> {
+    // `text` should not end with a newline.
+    Continue { game: G, text: String },
+    Goodbye { text: String },
+}
+
+impl<G> Output<G> {
+    pub fn text(&self) -> &str {
+        match self {
+            Output::Continue { text, .. } => text,
+            Output::Goodbye { text } => text,
+        }
     }
+
+    pub fn into_game(self) -> Option<G> {
+        if let Output::Continue { game, .. } = self {
+            Some(game)
+        } else {
+            None
+        }
+    }
+}
+
+pub fn run_game<G: GameBuilder>(game: G) -> io::Result<()> {
+    let mut first = true;
+    let mut stdin = io::stdin().lock();
+    let mut r = game.start();
+    loop {
+        if !std::mem::replace(&mut first, false) {
+            println!();
+        }
+        println!("{}", r.text());
+        let Some(game) = r.into_game() else {
+            break;
+        };
+        println!();
+        print!("> ");
+        let mut input = String::new();
+        stdin.read_line(&mut input)?;
+        r = game.handle_input(&input);
+    }
+    Ok(())
 }
